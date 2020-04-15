b83116dea5af9b240d8992e7ea2cbd1523d3d7a4
Ali-mszd <ali.moosazadeh92@gmail.com>
2020-04-16 00:00:25 +0430

    transition changed

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,67 +1,81 @@
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
 import Layout from "../components/layout";
-import React, {useEffect} from "react";
-import Breakpoint from 'react-socks';
-import axios from 'axios';
-import data from '../data/pictures';
+import React, { useEffect } from "react";
+import Breakpoint from "react-socks";
+import axios from "axios";
+import data from "../data/pictures";
 import Header from "../components/header";
 import Menu from "../components/menu";
-import {useLocation} from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import Gallery from "../components/gallery";
-import {useCurrentBreakpointName} from "react-socks";
-
+import { useCurrentBreakpointName } from "react-socks";
 
 function useQuery() {
-    return new URLSearchParams(useLocation().search);
+  return new URLSearchParams(useLocation().search);
 }
 
 const Home = () => {
-        const breakpoint = useCurrentBreakpointName();
-        let query = useQuery();
+  const breakpoint = useCurrentBreakpointName();
+  let query = useQuery();
 
-        /*        useEffect(() => {
+  /*        useEffect(() => {
                     /!*axios.get('../data/pictures.json').then(res => {
                         console.log(res.data)
                     })*!/
                 });*/
 
-        const transition = {
+  const transition = {
+    ease: "easeInOut",
+    duration: 1,
+    delay: 3,
+  };
 
-            ease: "easeInOut", duration:
-                1, delay:
-                3
-        };
+  const transitionSpring = {
+    type: "spring",
+    damping: 10,
+    stiffness: 100,
+  };
 
-        const transitionSpring = {
-            type: "spring",
-            damping: 10,
-            stiffness: 100
-        };
+  const fadeIn = {
+    visible: { opacity: 1 },
+    fade: { opacity: 0 },
+  };
 
-        const fadeIn = {
-            visible: {opacity: 1},
-            fade: {opacity: 0},
-        };
-
-        const type = query.get("type");
-        const activePic = query.get("pic");
-        return (
-            <Layout>
-                <Header breakpoint={breakpoint} data={data} transition={transitionSpring}/>
-                <motion.div initial={"fade"} animate={"visible"} variants={fadeIn}
-                            transition={transitionSpring}>
-                    <Breakpoint large down>
-                        <Gallery activePic={activePic} breakpoint={breakpoint} data={data} type={type}/>
-                        <Menu isLarge={false} type={type}/>
-                    </Breakpoint>
-                    <Breakpoint xlarge up>
-                        <Menu isLarge={true} type={type}/>
-                        <Gallery activePic={activePic} breakpoint={breakpoint} data={data} type={type}/>
-                    </Breakpoint>
-                </motion.div>
-            </Layout>
-        )
-    }
-;
-
-export default Home
+  const type = query.get("type");
+  const activePic = query.get("pic");
+  return (
+    <Layout>
+      <Header
+        breakpoint={breakpoint}
+        data={data}
+        transition={transitionSpring}
+      />
+      <motion.div
+        initial={"fade"}
+        animate={"visible"}
+        variants={fadeIn}
+        transition={transition}
+      >
+        <Breakpoint large down>
+          <Gallery
+            activePic={activePic}
+            breakpoint={breakpoint}
+            data={data}
+            type={type}
+          />
+          <Menu isLarge={false} type={type} />
+        </Breakpoint>
+        <Breakpoint xlarge up>
+          <Menu isLarge={true} type={type} />
+          <Gallery
+            activePic={activePic}
+            breakpoint={breakpoint}
+            data={data}
+            type={type}
+          />
+        </Breakpoint>
+      </motion.div>
+    </Layout>
+  );
+};
+export default Home;
