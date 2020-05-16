57e87002c7e5164d5648aef048e9736c224518df
Ali-mszd <ali.moosazadeh92@gmail.com>
2020-05-16 10:08:32 +0430

    animation mobile bug fixed

--- src/components/header.js
+++ src/components/header.js
@@ -1,66 +1,90 @@
 import React from "react";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 
 const Header = (props) => {
+  let isLarge = false;
 
-    let isLarge = false;
+  const slideDown = {
+    top: { marginTop: 0 },
+    center: { marginTop: "25vh" },
+  };
 
-    const slideDown = {
-        top: {marginTop: 0},
-        center: {marginTop: '25vh'},
+  const fadeIn = {
+    top: { opacity: 1 },
+    center: { opacity: 0 },
+  };
+
+  let heightControl = {
+    top: { maxHeight: "90vh" },
+    center: { maxHeight: "50vh" },
+  };
+
+  let blur = {
+    top: { opacity: 1 },
+    center: { opacity: 0 },
+  };
+
+  let headerImg = props.data.filter((pic) => pic.headerMd)[0];
+
+  if (props.breakpoint === "large" || props.breakpoint === "xlarge") {
+    heightControl = {
+      top: { maxHeight: "90vh" },
+      center: { maxHeight: "50vh" },
     };
-
-    const fadeIn = {
-        top: {opacity: 1},
-        center: {opacity: 0},
+    isLarge = true;
+    blur = {
+      top: { backdropFilter: "blur(4px)" },
+      center: { backdropFilter: "blur(0)" },
     };
+    headerImg = props.data.filter((pic) => pic.headerLg)[0];
+  }
 
-
-    let heightControl = {
-        top: {height: 'auto'},
-        center: {height: '50vh'},
-    };
-
-    let blur = {
-        top: {opacity: 1},
-        center: {opacity: 0},
-    };
-
-
-    let headerImg = props.data.filter(pic => pic.headerMd)[0];
-
-    if (props.breakpoint === 'large' || props.breakpoint === 'xlarge') {
-        heightControl = {
-            top: {maxHeight: '90vh'},
-            center: {maxHeight: '50vh'},
-        };
-        isLarge = true;
-        blur = {
-            top: {backdropFilter: 'blur(4px)'},
-            center: {backdropFilter: 'blur(0)'},
-        };
-        headerImg = props.data.filter(pic => pic.headerLg)[0];
-    }
-
-    return (
-        <motion.div initial={"center"} animate={"top"} variants={slideDown}
-                    transition={props.transition} className="position-relative">
-            <motion.div initial={"center"} animate={"top"} variants={heightControl}
-                        transition={props.transition} className="overflow-hidden">
-                <img src={headerImg.url} alt={headerImg.alt} className="img-fluid w-100"/>
-            </motion.div>
-            <motion.div initial={"center"} animate={"top"} variants={blur}
-                        transition={props.transition}
-                        className={(isLarge ? void 0 : "blur-sm") + " position-absolute w-100 h-100 top-0"}>
-            </motion.div>
-            <div
-                className="position-absolute d-flex align-items-center justify-content-center w-100 h-100 top-0">
-                <motion.h1 initial={"center"} animate={"top"} variants={fadeIn}
-                           transition={props.transition} className="text-white mb-0">Rainbow Galleria
-                </motion.h1>
-            </div>
-        </motion.div>
-    )
+  return (
+    <motion.div
+      initial={"center"}
+      animate={"top"}
+      variants={slideDown}
+      transition={props.transition}
+      className="position-relative"
+    >
+      <motion.div
+        initial={"center"}
+        animate={"top"}
+        variants={heightControl}
+        transition={props.transition}
+        className="overflow-hidden"
+      >
+        <img
+          src={headerImg.url}
+          alt={headerImg.alt}
+          className="img-fluid w-100"
+        />
+      </motion.div>
+      <motion.div
+        initial={"center"}
+        animate={"top"}
+        variants={blur}
+        transition={props.transition}
+        className={
+          (isLarge ? void 0 : "blur-sm") +
+          " position-absolute w-100 h-100 top-0"
+        }
+      ></motion.div>
+      <div className="position-absolute d-flex align-items-center justify-content-center w-100 h-100 top-0">
+        <div>
+          <motion.h1
+            initial={"center"}
+            animate={"top"}
+            variants={fadeIn}
+            transition={props.transition}
+            className="text-white mb-0"
+          >
+            Rainbow Galleria
+          </motion.h1>
+        </div>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Header;
