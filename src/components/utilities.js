99b582ab0600affa677bf7e5c527c64dfa3f4e5a
Ali-mszd <ali.moosazadeh92@gmail.com>
2020-05-18 02:20:01 +0430

    mojtaba

--- src/components/utilities.js
+++ src/components/utilities.js
@@ -1,19 +1,14 @@
-import {BreakpointProvider as Breakpoint} from 'react-socks';
+import { BreakpointProvider as Breakpoint } from "react-socks";
 import useWindowDimensions from "./viewport";
-import {withRouter} from 'react-router-dom';
-import React, {useEffect} from 'react';
+import { withRouter } from "react-router-dom";
+import React, { useEffect } from "react";
 
-
-const Utilities = ({children}) => {
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    },[]);
-    useWindowDimensions();
-    return (
-        <Breakpoint>
-            {children}
-        </Breakpoint>
-    )
+const Utilities = ({ children }) => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  useWindowDimensions();
+  return <Breakpoint>{children}</Breakpoint>;
 };
 
 export default withRouter(Utilities);
